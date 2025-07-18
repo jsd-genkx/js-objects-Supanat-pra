1f28f0a13ed136ddefecd1fd91da4ef979544554
Supanat-pra <earthpra06@gmail.com>
2025-07-18 10:39:49 +0700

    objects solution

--- objects.js
+++ objects.js
@@ -28,37 +28,57 @@ let cart = {
 // Task 01: CRUD on an Object (After each step, log the product object.)
 // 1️⃣ C – Create (Add new property)
 // 👉 Add stock: true to product.
-
+	product.stock = true;
+	console.log(product);
 // 2️⃣ R – Read (Access property)
 // 👉 Log product.details.brand and product.details.specs.cpu.
-
+	console.log(product.details.brand);
+	console.log(product.details.specs.cpu);
 // 3️⃣ U – Update (Change value)
 // 👉 Update product.price to 999.
-
+	product.price = 999;
+	console.log(product.price);
 // 4️⃣ D – Delete (Remove property)
 // 👉 Delete product.details.specs.ram.
+	delete product.details.specs.ram;
+	console.log(product);
 
 // Task 02: Copying Objects
 // 5️⃣ Shallow copy (spread operator)
 // 👉 Create productCopy = { ...product }.
 // Change productCopy.name = "Gaming Laptop".
 // ✅ Log both product.name and productCopy.name.
+	let productCopy = {...product};
+	productCopy.name = "Gaming Laptop";
+	console.log(product.name);
+	console.log(productCopy.name);
 
 // 6️⃣ Deep copy (structuredClone)
 // 👉 Create cartCopy = structuredClone(cart).
 // Change cartCopy.customer.address.city = "Chiang Mai".
 // ✅ Log cart.customer.address.city and cartCopy.customer.address.city.
+	let cartCopy = structuredClone(cart);
+	cartCopy.customer.address.city = "Chiang Mai";
+	console.log(cart.customer.address.city);
+	console.log(cartCopy.customer.address.city);
 
 // Task 03: Destructuring
 // 7️⃣ Basic destructuring
 // 👉 Extract name and price from product into variables.
 // ✅ Log name and price.
+	const {name, price} = product;
+	console.log(name, price);
 
 // 8️⃣ Rename and default
 // 👉 Destructure product so that name becomes productName.
 // 👉 Also give stock a default value of false if it’s not in the object.
 // ✅ Log productName and stock.
+	const {name:productName, stock=false} = product;
+	console.log(productName);
+	console.log(stock)
 
 // 9️⃣ Nested destructuring
 // 👉 From product.details, extract brand and from product.details.specs, extract cpu in one destructuring statement.
 // ✅ Log brand and cpu.
+	const {details:{brand,specs:{cpu}}} = product;
+	console.log(cpu);
